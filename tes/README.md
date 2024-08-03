201db7cb8cbdd835c043b197d1fa8b948c2f5f28
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2024-08-03 16:19:54 -0300

    feat: fisrt commit

---
+++ tes/README.md
@@ -0,0 +1,6 @@
+<style>
+    h1 {
+     align-items: center;
+    }
+</style>
+<h1>Hello<h1/>
---
+++ tes/exemplo.js
@@ -0,0 +1,25 @@
+//var e let variam, já o const e constante 
+var a 
+let b
+const c = 5 
+
+a = 'banana'
+b = 404
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+//criar uma função para somar numeros
+function SomarNumeros(num1, num2) {
+    return num1 + num2
+}
+
+console.log(SomarNumeros(20,100))
+
+function QuantidadeCaracter(palavra) {
+    console.log(palavra, 'dentro')
+    return palavra
+}
+
+console.log(QuantidadeCaracter('lucas'))
